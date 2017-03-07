25921280f457fbdb060bb3a91948d44ade00f8cf
DavidPerezOrfila <knightbcn@gmail.com>
2017-03-07 17:22:37 +0100

    base project

--- src/AppBundle/Controller/postController.php
+++ src/AppBundle/Controller/postController.php
@@ -12,7 +12,6 @@ use AppBundle\Entity\Post;
 use AppBundle\Form\postType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class postController extends Controller
 {
@@ -41,24 +40,5 @@ class postController extends Controller
             'form' => $form->createView(),
         ]);
     }
-    /**
-     * @Route("/do-add-post", name="app_post_doAdd")
-     */
-    public function doAddPostAction(Request $request)
-    {
-        $a = new Post();
-        $form = $this->createForm(new postType(), $a);
-        $form->handleRequest($request);
-        if ($form->isValid()) {
-            $m = $this->getDoctrine()->getManager();
-            $tagRepo = $m->getRepository('AppBundle:Tag');
-            $tagRepo->addTagsIfAreNew($a);
-            $m->persist($a);
-            $m->flush();
-            return $this->redirectToRoute('app_index_articles');
-        }
-        return $this->render(':Post:AddPost.html.twig', [
-            'form' => $form->createView(),
-        ]);
-    }
+
 }
